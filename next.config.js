640b55b3840b00a7a35a81d06d0d2ef9af5fbe9b
luizctb <luizcarlostb@hotmail.com>
2023-08-31 22:34:35 -0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    pageExtensions: ['gage.tsx', 'api.ts', 'api.tsx'],
+}
 
 module.exports = nextConfig
